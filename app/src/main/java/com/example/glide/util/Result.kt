ed5b1ebf283dd88e0a59e48b64ec79a93370f88f
sahilsharma <sahilsharma75831@gmail.com>
2025-09-21 08:20:47 +0530

    branch initialized

--- app/src/main/java/com/example/glide/util/Result.kt
+++
@@ -1,8 +0,0 @@
-package com.example.glide.util
-
-
-sealed class AuthResult<out T> {
-    data class Success<out T>(val data: T): AuthResult<T>()
-    data class Error(val message: String, val exception: Exception? = null) : AuthResult<Nothing>()
-    object Loading : AuthResult<Nothing>()
-}
